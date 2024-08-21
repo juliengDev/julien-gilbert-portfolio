cb9ac03029869585d69c7f9c3c3efa992b95e564
Julien Gilbert <juliengilbert.dev@gmail.com>
2024-08-21 15:07:20 +0700

    dialogue logic added

--- src/main.js
+++ src/main.js
@@ -38,6 +38,31 @@ k.scene("main", async () => {
     },
     "player",
   ]);
+
+  for (const layer of layers) {
+    if (layer.name === "boundaries") {
+      for (const boundary of layer.objects) {
+        map.add([
+          k.area({
+            shape: new k.Rect(k.vec2(0), boundary.width, boundary.height),
+          }),
+          k.body({ isStatic: true }),
+          k.pos(boundary.x, boundary.y),
+          boundary.name,
+        ]);
+
+        if (boundary.name) {
+          player.onCollide(boundary.name, () => {
+            player.isInDialogue = true;
+            displayDialogue(
+              dialogueData[boundary.name],
+              () => (player.isInDialogue = false)
+            );
+          });
+        }
+      }
+    }
+  }
 });
 
 k.go("main");
